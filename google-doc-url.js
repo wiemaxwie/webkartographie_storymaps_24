bbbfe3cf9433a55261ee3c1c61a4da528446937c
wiemaxwie <109140300+wiemaxwie@users.noreply.github.com>
2024-06-14 16:41:20 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1t0w1lTyzQKZPdw9nxrQ0f4K0XabxLrVczv66blbLA2A/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyAtJarj0kmrj8KxD8kHrTjwhsEMiCTwJ94';
